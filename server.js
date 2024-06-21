0eec205a8ffc714f421fd1a3945b4e330399f0ff
Rafael Peña <rrafapm@gmail.com>
2024-06-21 19:03:15 +0200

    repaso

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ dotenv.config({path: '.config/config.env'});
 // Crear opbjeto app a partir de la clase express
 const app = express();
 
+// Enlace  a fichero de routes
+const campus = require('./routes/campus');
+
+// Montar(activar) rutas
+app.use('/api/v1/campus', campus);
+
 // Determinar el puerto de ejecución del server
 const PORT = process.env.PORT || 5000;
 
